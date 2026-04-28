1c43c667f866678eefd15216bfe557168f946e77
Imaginary Seb <alice.liddell.in.wonderland.13@gmail.com>
2026-04-28 22:02:20 +0000

    Delete testing/target directory

--- testing/target/debug/deps/testing-8568f79d0e3f9d0e.d
+++
@@ -1,5 +0,0 @@
-/home/alice/git/test/testing/target/debug/deps/testing-8568f79d0e3f9d0e.d: src/main.rs
-
-/home/alice/git/test/testing/target/debug/deps/libtesting-8568f79d0e3f9d0e.rmeta: src/main.rs
-
-src/main.rs:
--- testing/target/debug/deps/testing-8620cfdad00e9e82.d
+++
@@ -1,5 +0,0 @@
-/home/alice/git/test/testing/target/debug/deps/testing-8620cfdad00e9e82.d: src/main.rs
-
-/home/alice/git/test/testing/target/debug/deps/libtesting-8620cfdad00e9e82.rmeta: src/main.rs
-
-src/main.rs:
